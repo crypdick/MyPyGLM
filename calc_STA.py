0ad71041a31854386c3d489b93e2521a3536aed1
rdecal <decal@uw.edu>
2015-02-11 21:22:01 -0800

    initial commit

---
+++ calc_STA.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spike Triggered Average calculator
+
+Input:
+stimulus (t)
+spike times (t)
+
+output:
+averaged stimulus before the spiketime
+
+
+Created on Wed Feb 11 21:20:00 2015
+@author: Richard Decal, decal@uw.edu
+"""
+
