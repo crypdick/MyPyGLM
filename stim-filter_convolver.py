d8c884f74f7b9e7953f6602a6cd01d27275f76d6
rdecal <decal@uw.edu>
2015-02-26 15:48:09 -0800

    added some pseudocode

--- stim-filter_convolver.py
+++ stim-filter_convolver.py
@@ -2,6 +2,16 @@
 """
 Convolve filter with stimulus
 
+input:
+stimulus
+filter
+
+output:
+convolved stimulus
+
+TODO: create dummy convolver that filters a delta function to get an impulse response
+that looks like the filter
+
 Created on Wed Feb 11 22:31:01 2015
 @author: Richard Decal, decal@uw.edu
 """
